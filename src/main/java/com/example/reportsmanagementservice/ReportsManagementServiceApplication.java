b725d3ef392d5f512b64b4a35bf73de621061db6
vandalv <64989065+vandalv@users.noreply.github.com>
2022-05-18 10:29:28 +0200

    Add files via upload
    -v1

---
+++ src/main/java/com/example/reportsmanagementservice/ReportsManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reportsmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportsManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reportsmanagementservice/ReportsManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reportsmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
